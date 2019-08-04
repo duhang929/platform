cf9b4fbf9efff2b9e81330d3e51a25534f39608b
limaoyuan0204 <673897053@qq.com>
2019-08-04 16:30:09 +0800

    测试提交、、、、.。。。。、。。。。。

--- eim-authority/src/main/java/com/stars/authority/controller/user/UserInfoController.java
+++ eim-authority/src/main/java/com/stars/authority/controller/user/UserInfoController.java
@@ -16,7 +16,7 @@ public class UserInfoController extends BaseController{
 
     private UserService userService;
 
-    // tests 324234
+    // tests 324234 324
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
