04bcbfb71e25cc3a2b42e0456481dfbaa8326eaf
duhang <944502937@qq.com>
2019-08-04 15:57:46 +0800

    修改配置

--- eim-authority/src/main/java/com/stars/authority/controller/user/UserInfoController.java
+++ eim-authority/src/main/java/com/stars/authority/controller/user/UserInfoController.java
@@ -1,11 +1,28 @@
 package com.stars.authority.controller.user;
 
 import com.stars.authority.controller.BaseController;
+import com.stars.authority.model.UserInfo;
+import com.stars.authority.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(value = "/user")
 public class UserInfoController extends BaseController{
 
+    private UserService userService;
 
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 
+    @GetMapping(value = "/info")
+    public List<UserInfo> get(){
+        return userService.searchUsers();
+    }
 }
